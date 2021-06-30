05f863b7f08d77483cf43f3f21bb71ec4b435287
Ridam Hazra <rhazra0602@gmail.com>
2021-06-30 17:33:56 +0530

    Add files via upload

---
+++ sketcher.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+def dodgeV2(image, mask):
+    return cv2.divide(image, 255-mask, scale=256)
+
+## read image
+
+imgpath = ''
+img = cv2.imread('imagefilename.filetype')
+
+## detect edges
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gray_inv = 255 - gray
+img_blur = cv2.GaussianBlur(gray_inv, ksize=(21, 21),sigmaX=0, sigmaY=0)
+img_blend = dodgeV2(gray, img_blur)
+img_blend *= 12
+
+## save image
+  
+outpath = ''
+cv2.imwrite('newimagefilename.filetype',img_blend)
